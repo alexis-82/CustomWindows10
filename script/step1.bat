7a2a178c528bfec2f6c57eefd3fe7c898f2cb732
alexis-82 <giobbia82@gmail.com>
2022-11-25 16:33:59 +0100

    new add

---
+++ script/step1.bat
@@ -0,0 +1,13 @@
+echo off
+cls
+
+setlocal enabledelayedexpansion
+set fore_dred=[31m
+echo.
+echo.
+echo [31mAccount Administrator ABILITATO![0m
+net user Administrator /active:yes
+echo.
+echo.
+set/p "end=Premere un tasto per disconnettere l'utente!"
+logoff
