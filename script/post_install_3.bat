c795ea7ebc8e93f21dac63f5346997e2d67a1dfa
alexis-82 <alessioabrugiati@gmail.com>
2022-11-26 15:47:04 +0100

    new add

---
+++ script/post_install_3.bat
@@ -0,0 +1,18 @@
+echo off
+cls
+
+setlocal enabledelayedexpansion
+set fore_dred=[31m
+set underline=[4m
+echo.
+echo.
+cd C:\Users
+rmdir /s /q Administrator
+echo.
+echo.
+set/p "end=[31mPulizia effettuata, premere un tasto![0m"
+exit
+
+
+
+
